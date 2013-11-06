745edb213f28d00b22c3096961ccf398307d6a25
TeamMutiny <c111957@drdrb.com>
2013-11-06 15:45:58 +0200

    pieniä fixejä -J

---
+++ Assets/LocalPositionAnimation.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+ 
+
+[RequireComponent(typeof(Animation))]
+
+public class LocalPositionAnimation : MonoBehaviour
+
+{
+
+    Vector3 localPos;
+
+    bool wasPlaying;
+
+ 
+
+    void Awake()
+
+    {
+
+        localPos = transform.position;
+
+        wasPlaying = false;
+
+    }
+
+ 
+
+    void LateUpdate()
+
+    {
+
+        if (!animation.isPlaying && !wasPlaying)
+
+            return;
+
+ 
+
+        transform.localPosition += localPos;
+
+ 
+
+        wasPlaying = animation.isPlaying;
+
+    }
+
+}
