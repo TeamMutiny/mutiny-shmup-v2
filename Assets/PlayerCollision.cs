58ce6ba4520e9a95c1640270d334c96e4f457d10
Ville Touronen <touronen@gmail.com>
2013-11-06 19:26:57 +0200

    Collision detection for enemy bullets. -V

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -17,5 +17,10 @@ public class PlayerCollision : MonoBehaviour {
 		if(other.gameObject.tag == "Enemy") {
 			GameObject.Find("HP bar").SendMessage("GotHit", 5.0f);
 		}
+		
+		if(other.gameObject.tag == "EnemyProjectile") {
+			GameObject.Find("HP bar").SendMessage("GotHit", 5.0f);
+			Destroy(other.gameObject);
+		}
 	}
 }
